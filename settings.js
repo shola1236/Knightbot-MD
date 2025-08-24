d00affe6352495eed897e9729b382e2ceb7bbf14
shola1236 <akinosifadeshola@gmail.com>
2025-08-24 12:53:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'sholaupates', // Your name
   ownerNumber: '2349028694300', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.9",
 };
